9fb34adf2cca207ffed4ae6b2dd73c13a7fac81b
Marten Kerkhofs <martenkerkhofs@hotmail.com>
2019-06-04 11:03:22 +0200

    optimised getLeftOrRight.R by removing most of the loop, avoiding doing the same thing multiple times. e.g. instead of 40 iterations, it only does 5 (unique) iterations for gene 1, mostly at the same time as well. Also made the scripts run on my MAC.

---
+++ src_Metabolite_Set_Creation/Crossomics/build_mets_set/getMetsPathwayCommons.R
@@ -0,0 +1,125 @@
+getMetsPathwayCommons <- function(id_uniprot, src){
+# id_uniprot="Q9UM01"
+
+  # require("paxtoolsr")
+  # require("SPARQL")
+  
+  # library("biomaRt")
+  # ls("package:paxtoolsr")
+
+  # ensembl = tryCatch(
+  #   { useMart("ensembl",dataset="hsapiens_gene_ensembl") }
+  #   , error = function(e) {
+  #     message(paste("CATCHED", e))
+  #     message("Use other server!")
+  #     useMart("ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "grch37.ensembl.org")
+  #   })
+
+  # listDatasets(mart = ensembl)
+  # listAttributes(ensembl)
+
+  # id_uniprot = idMapping(toString(entrezgene), verbose = TRUE)
+  # id <- idMapping("HEXA", verbose = TRUE)
+  # searchResults <- searchPc(q = "glycolysis", type = "pathway")
+  # id_uniprot ="P10636"
+
+  searchResults = NULL
+#####################################################################################  
+  
+  try({searchResults = searchPc(q = toString(id_uniprot), organism = "homo sapiens", type = "Catalysis")}, silent = TRUE)
+  path = "Catalysis/controlled"
+  if (is.null(searchResults)){
+    message("No search results for Catalysis, try Control!!!")
+    try({searchResults = searchPc(q = toString(id_uniprot), organism = "homo sapiens", type = "Control")}, silent = TRUE)
+    path = "Control/controlled"
+    if (is.null(searchResults)){
+      return(NULL)
+    }  
+  }
+  
+  uris = xpathSApply(searchResults, "/searchResponse/searchHit/uri", xmlValue)
+  
+  leftMets=NULL
+  rightMets=NULL
+  xml = NULL
+  
+  try({xml = traverse(uri = uris, path = path)}, silent = TRUE)
+  uris2 = xpathSApply(xml, "//value/text()", xmlValue)
+
+  # Transport
+  try({xml2 = traverse(uri = uris2, path = "Transport/left:SmallMolecule")}, silent = TRUE)
+  uris3 = xpathSApply(xml2, "//value/text()", xmlValue)
+  if (!is.null(uris3)){
+    leftMets = rbind(leftMets, getLeftOrRight(uris3, entrezgene, "left"))
+  }
+  try({xml2 = traverse(uri = uris2, path = "Transport/right:SmallMolecule")}, silent = TRUE)
+  uris3 = xpathSApply(xml2, "//value/text()", xmlValue)
+  if (!is.null(uris3)){
+    rightMets = rbind(rightMets, getLeftOrRight(uris3, entrezgene, "right"))
+  }
+  
+  # BiochemicalReaction
+  try({xml2 = traverse(uri = uris2, path = "BiochemicalReaction/left:SmallMolecule")}, silent = TRUE)
+  uris3 = xpathSApply(xml2, "//value/text()", xmlValue)
+  if (!is.null(uris3)){
+    leftMets = rbind(leftMets, getLeftOrRight(uris3, entrezgene, "left"))
+  }
+  try({xml2 = traverse(uri = uris2, path = "BiochemicalReaction/right:SmallMolecule")}, silent = TRUE)
+  uris3 = xpathSApply(xml2, "//value/text()", xmlValue)
+  if (!is.null(uris3)){
+    rightMets = rbind(rightMets, getLeftOrRight(uris3, entrezgene, "right"))
+  }
+
+  # # owl = getPc(uris[2])
+  # # saveXML(owl,file="reactome.owl")
+  # 
+  # # Chebi parent to child relaties
+  # chebi = read.csv("compounds.tsv",header=TRUE, sep="\t")
+  # 
+  # entry = which(chebi[,"CHEBI_ACCESSION"]=="CHEBI:15705")
+  # chebi[which(chebi[,"PARENT_ID"]==chebi[entry,"ID"]), ]
+  # 
+  # entry = which(chebi[,"CHEBI_ACCESSION"]=="CHEBI:16467")
+  # chebi[which(chebi[,"PARENT_ID"]==chebi[entry,"ID"]), ]
+  
+  
+  # , ns=c("time","<http://www.w3.org/2006/time#>")
+  
+
+  # http://purl.obolibrary.org/obo/chebi.owl
+  
+  # endpoint="http://chebi.bio2rdf.org/sparql"
+  # 
+  # 
+  # endpoint="http://dbpedia.org/sparql"
+  # d = SPARQL(url=endpoint,
+  #            query="SELECT DISTINCT ?concept WHERE {?subject a ?concept .} LIMIT 50")
+  # 
+  # 
+  # d = SPARQL(url=endpoint,
+  #   query="PREFIX chebi1: <http://purl.obolibrary.org/obo/chebi#2>
+  #          PREFIX chebi2: <http://purl.obolibrary.org/obo/chebi#3>
+  #          PREFIX chebi3: <http://purl.obolibrary.org/obo/chebi#1>
+  #          PREFIX chebi: <http://purl.obolibrary.org/obo/chebi#>
+  #          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  #          PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  #          PREFIX oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
+  #          PREFIX xml: <http://www.w3.org/XML/1998/namespace>
+  #          PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+  #          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  #          PREFIX obo: <http://purl.obolibrary.org/obo/>
+  #          SELECT * WHERE { ?IAO_0000115 oboInOwl:id CHEBI:15705.} LIMIT 10")
+  # is.data.frame(d$results)
+                   
+
+  resources = unique(c(leftMets[,"Data Source"],rightMets[,"Data Source"]))
+  rxns = NULL
+  for (i in 1:length(resources)){
+    rxn = paste(paste(leftMets[which(leftMets[,"Data Source"]==resources[i]), "Compound"], collapse = " + "),
+                paste(rightMets[which(rightMets[,"Data Source"]==resources[i]), "Compound"], collapse = " + "),sep=" <=> ")
+    names(rxn) = resources[i]
+    rxns = c(rxns,rxn)
+  }
+  
+  return(list("left"=leftMets, "right"=rightMets, "rxns"=rxns))
+}
---
+++ src_Metabolite_Set_Creation/findSurroundings_Marten.R
@@ -0,0 +1,76 @@
+run <- function(entry, outdir, src){
+# entry=1
+# outdir="./results"
+# src="./src"
+  
+# src="/hpc/shared/dbg_mz/marcel/Crossomics_Build_Metabolite_Set/src"
+# outdir="/hpc/shared/dbg_mz/marcel/Crossomics_Build_Metabolite_Set/results"
+  
+  source(paste(src, "Crossomics/sourceDir.R", sep="/"))
+  sourceDir(paste(src, "Crossomics/build_mets_set", sep="/"))
+  
+  dir.create(outdir, showWarnings = F)
+  dir.create(paste(outdir, "mss", sep="/"), showWarnings = F)
+  
+  # entry=which(HGNC2Uniprot[,"hgnc_symbol"]=="SLC7A7")
+
+  # dim(HGNC2Uniprot)
+  # length(unique(HGNC2Uniprot[,"uniprotswissprot"]))
+  # length(unique(HGNC2Uniprot[,"hgnc_symbol"]))
+
+  # load(paste(src, "HGNC2Uniprot_prev.RData", sep="/"))
+  load(paste(src, "HGNC2Uniprot.RData", sep="/"))
+  
+  cat("HGNC loaded\n") # Check for where the script is
+  
+  id_uniprot = HGNC2Uniprot[entry,"uniprotswissprot"]
+  hgnc = HGNC2Uniprot[entry,"hgnc_symbol"]
+  
+  cat("HGNC:", hgnc, "\n") # Check for where the script is
+    
+  rval = getMetsPathwayCommons(id_uniprot = id_uniprot, src = src)
+  
+  outdir=paste(outdir, "mss", sep="/")
+  if (!is.null(rval$left) | !is.null(rval$right)){
+    
+    f = paste(outdir, paste0(hgnc, ".RData"), sep="/")
+    n=1
+    while(file.exists(f)){
+      n = n+1
+      f = unlist(strsplit(f,".",fixed=TRUE))
+      f = f[length(f)-1]
+      # if (length(grep("_",f,fixed=TRUE))>0) f=unlist(strsplit(f,"_",fixed=TRUE))[1]
+      if (grepl("_(?!.*/)", f, perl =TRUE)) f=unlist(strsplit(f, "_(?=[0-9])", perl=TRUE))[1]
+      f = paste0(paste(f,n,sep="_"),".RData")
+    }
+    save(rval, file=f)
+  }
+}
+      
+message("Start")
+cat("==> reading arguments:\n", sep = "")
+
+# New edit (31-05-19), loading all required packages beforehand:{
+library(SPARQL)
+library(rJava)
+library(XML)
+library(paxtoolsr)
+library(RCurl)
+library(bitops)
+# }
+
+cmd_args = commandArgs(trailingOnly = TRUE)
+
+# TEST code{
+# cmd_args <- c("2", "/Users/mkerkho7/DIMS2_repo/Crossomics/Results", "/Users/mkerkho7/DIMS2_repo/Crossomics/src/src_Metabolite_Set_Creation")
+#}
+
+for (arg in cmd_args) cat("  ", arg, "\n", sep="")
+
+run(entry = as.numeric(cmd_args[1]), outdir = cmd_args[2], src = cmd_args[3])
+
+message("Ready")
+
+
+
+
