3e495c68eff60a39ef3f14a6a93d0651d81c7a4b
Marten Kerkhofs <martenkerkhofs@hotmail.com>
2019-06-04 11:04:46 +0200

    restructured folder for clearer distinction of creation and usage of metabolite sets.

--- src Metabolite Set Creation/Crossomics/build_mets_set/convert.R
+++
@@ -1,7 +0,0 @@
-convert <- function(a) {as.vector(unlist(lapply(a, function(x) {
-  if (identical(unlist(x),character(0))){
-    return(NA)
-  } else {
-    return(x)
-  }
-})))}
--- src Metabolite Set Creation/run_array_job.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# load(paste(src, "HGNC2Uniprot.RData", sep="/"))
-# dim(HGNC2Uniprot) => 19266
-
-scripts="/hpc/shared/dbg_mz/marcel/Crossomics_Build_Metabolite_Set/src"
-
-qsub -l h_rt=08:00:00 -l h_vmem=16G -t 1-22968:1 -tc 50 $scripts/array_job.sh
