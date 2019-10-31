c8646d843172b2e2c99196c047b54c62a783b79b
Marten Kerkhofs <martenkerkhofs@hotmail.com>
2019-10-31 17:22:28 +0100

    file folder needs to be changed manually and the resulting RData file will be put in the exact same folder as the original file

--- src_Metabolite_mapper/Convert_xlsx_to_RDS.R
+++ src_Metabolite_mapper/Convert_xlsx_to_RDS.R
@@ -1,6 +1,6 @@
 # Convert xlsx bioinformatics file to RDS
 
-# Go to some directory where the xlsx file is (only 1 xlsx file should be present) and run the whole thing
+# Go to any directory where the xlsx file is (only 1 xlsx file should be present) and run the whole thing
 setwd("/Users/mkerkho7/DIMS2_repo/Crossomics/Data/Project 2017_008 MetabolomicsDiagnosis_DBS/RES_DBS_20170420_MetabolomicsDiagnosis_RUN2/Bioinformatics_metIS/")
 
 name_xlsx <- list.files(pattern = "*.xlsx")
