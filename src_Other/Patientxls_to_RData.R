36416133e52bd3b9bf99dab4739c4f3abe1efea3
Marten Kerkhofs <martenkerkhofs@hotmail.com>
2019-12-05 14:13:59 +0100

    changed order to be more readable and commented out unnecessary piece

--- src_Other/Patientxls_to_RData.R
+++ src_Other/Patientxls_to_RData.R
@@ -31,7 +31,33 @@ name_new_patient_data <- "Crossomics_DBS_Marten_trimmed20191204"
 
 
 
-##### Make RData file from xlsx file
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Functions ---------------------------------------------------------------
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+fix_patient_number <- function(xls_data, old_patient_column, DBS = TRUE){
+  if(DBS){
+    tmp <- sapply(strsplit(xls_data[, old_patient_column],"[P.]"), function(x)
+      paste0("P", sprintf("%03d",as.numeric(x[2])), ".", x[3])
+    )
+  } else {
+    tmp <- sapply(strsplit(xls_data[, old_patient_column],"[P.]"), function(x)
+      paste0("P", sprintf("%03d",as.numeric(x[2])))
+    )
+  }
+  return(tmp)
+}
+get_PatientID <- function(xls_data, column1, column2){
+  patientID <- paste(xls_data[,column1], xls_data[,column2], sep = "^")
+  return(patientID)
+}
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Make RData file from xlsx file ------------------------------------------
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 path_of_xls <- paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/../Data/")
 xls_df <- read.xlsx(paste0(path_of_xls,name_patient_data,".xlsx"), sheetIndex = 1)
 xls_df <- xls_df[,colSums(is.na(xls_df))<nrow(xls_df)]
@@ -51,27 +77,6 @@ if(sum(colnames(xls_data) == "Patient number in set" | colnames(xls_data) == "Pa
   colnames(xls_data)[colnames(xls_data) == "Patient.number.in.set.Iden2"] <- "Old.patient.number.Iden2"
   colnames(xls_data)[colnames(xls_data) == "Patient number in set.Iden3"] <- "Old.patient.number.Iden3"
   colnames(xls_data)[colnames(xls_data) == "Patient.number.in.set.Iden3"] <- "Old.patient.number.Iden3"
-}
-
-
-##### 
-# Functions
-
-fix_patient_number <- function(xls_data, old_patient_column, DBS = TRUE){
-  if(DBS){
-    tmp <- sapply(strsplit(xls_data[, old_patient_column],"[P.]"), function(x)
-      paste0("P", sprintf("%03d",as.numeric(x[2])), ".", x[3])
-    )
-  } else {
-    tmp <- sapply(strsplit(xls_data[, old_patient_column],"[P.]"), function(x)
-      paste0("P", sprintf("%03d",as.numeric(x[2])))
-    )
-  }
-  return(tmp)
-}
-get_PatientID <- function(xls_data, column1, column2){
-  patientID <- paste(xls_data[,column1], xls_data[,column2], sep = "^")
-  return(patientID)
 }
 
 ##### 
@@ -102,18 +107,18 @@ xls_data[xls_data == "NA^NA"] <- NA
 save(file = paste0(path_of_xls,name_new_patient_data,".RData"), xls_data)
 
 
-
-##### Check disease genes and print which ones don't have a metabolite set
-dum_list <- list.files(paste0(path_of_xls, date_of_metsets))
-smal_maxrxn <- paste0("/maxrxn",sort(as.numeric(unlist(strsplit(dum_list[grep("maxrxn", dum_list)], split = "maxrxn"))))[1], "/mss_0")
-avail_genes <- unlist(strsplit(list.files(paste0(path_of_xls,date_of_metsets,smal_maxrxn)), ".RDS"))
-
-gene_col <- grep("Gene", colnames(xls_DT), value = TRUE)
-if(length(gene_col) != 1){
-  stop("There are multiple or no columns with the term 'Gene' as name. Check whether a disease gene column is present and change its name")
-} else {
-  disease_genes <- unique(trimws(unlist(strsplit(as.character(xls_DT[[gene_col]]), split = ";"))))
-  missing_genes <- disease_genes[!disease_genes %in% avail_genes]
-  message("these genes are listed in the patient dataset, but not present as having a metabolite set:\n", paste(missing_genes, collapse = "\n"))
-}
-
+# 
+# ##### Check disease genes and print which ones don't have a metabolite set
+# dum_list <- list.files(paste0(path_of_xls, date_of_metsets))
+# smal_maxrxn <- paste0("/maxrxn",sort(as.numeric(unlist(strsplit(dum_list[grep("maxrxn", dum_list)], split = "maxrxn"))))[1], "/mss_0")
+# avail_genes <- unlist(strsplit(list.files(paste0(path_of_xls,date_of_metsets,smal_maxrxn)), ".RDS"))
+# 
+# gene_col <- grep("Gene", colnames(xls_DT), value = TRUE)
+# if(length(gene_col) != 1){
+#   stop("There are multiple or no columns with the term 'Gene' as name. Check whether a disease gene column is present and change its name")
+# } else {
+#   disease_genes <- unique(trimws(unlist(strsplit(as.character(xls_DT[[gene_col]]), split = ";"))))
+#   missing_genes <- disease_genes[!disease_genes %in% avail_genes]
+#   message("these genes are listed in the patient dataset, but not present as having a metabolite set:\n", paste(missing_genes, collapse = "\n"))
+# }
+# 
