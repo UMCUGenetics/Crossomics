0116730cf8e74c435e9054e33d5843d9dd702b67
Marten Kerkhofs <martenkerkhofs@hotmail.com>
2020-01-08 14:39:29 +0100

    updated to latest naming convention and that it can be run independently

--- src_After_Analysis/Distribution_Crossomics_vs_Random.R
+++ src_After_Analysis/Distribution_Crossomics_vs_Random.R
@@ -6,8 +6,11 @@ library(ggplot2)
 
 # Date of data
 date <- "2019-12-10"
+
+code_dir <- paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/../Results/")
 outdir_name <- paste0(dirname(rstudioapi::getActiveDocumentContext()$path),"/../Plots/",date)
 
+DT_per_parameter_tra_val <- readRDS(paste0(code_dir,date,"/MSEA_DT_per_parameter_tra_val.RDS"))
 
 distribution <- NULL
 for(k in c(1:10)){
@@ -30,20 +33,20 @@ for(k in c(1:10)){
 
 distribution_df <- data.frame("nr_success" = distribution)
 
-steps <- unique(DT_per_parameter$Step)
-threshs <- unique(DT_per_parameter$Z_threshold)
-maxrxns <- unique(DT_per_parameter$Max_rxn)
+steps <- unique(DT_per_parameter_tra_val$Step)
+threshs <- unique(DT_per_parameter_tra_val$Z_threshold)
+maxrxns <- unique(DT_per_parameter_tra_val$Max_rxn)
 
 p <- ggplot(distribution_df) + 
   geom_density(aes(x = nr_success, y=..scaled.., colour = "Random"))
 for(st in steps){
   for(thr in threshs){
     for(mx in maxrxns){
-      unique_values <- unique(DT_per_parameter[Validation == FALSE & Step == 0 & Z_threshold == thr & Max_rxn == mx, Prior.frac10])
+      unique_values <- unique(DT_per_parameter_tra_val[Validation == FALSE & Step == 0 & Z_threshold == thr & Max_rxn == mx, Prior.frac10])
       if(length(unique_values) == 1){
         next()
       }
-      p <- p + geom_density(data = DT_per_parameter[Validation == FALSE & Step == st & Z_threshold == thr & Max_rxn == mx, ], 
+      p <- p + geom_density(data = DT_per_parameter_tra_val[Validation == FALSE & Step == st & Z_threshold == thr & Max_rxn == mx, ], 
                             aes(x = Prior.frac10, y=..scaled.., colour = "Cross-omics"))
     }
   }
@@ -54,10 +57,7 @@ p <- p + theme_light() +
   scale_x_continuous("Ratio in top 10", breaks = seq(0,0.7,0.1), limits = c(0, 0.7)) +
   ylab("Distribution") +
   labs(color = "Method") 
-  # ggtitle("Comparison performance random vs Cross-omics")
 
-ggsave(paste0(outdir_name,"/Cross-omics_vs_random_performance.png"), plot = p,
-       width = 200, height = 50, dpi=300, units = "mm")
+ggsave(paste0(outdir_name,"/Cross-omics_vs_random_performance.svg"), plot = p,
+       width = 200, height = 50, units = "mm")
   
-  # geom_vline(xintercept = DT_per_parameter[Validation == FALSE, Av.prior.frac10], colour = 'red') +
-
